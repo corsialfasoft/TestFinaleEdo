352e1626cb85635a5a99c6cfa2306f3d3af6a80f
JokerJackassJam <edo.bevione@gmail.com>
2018-05-09 17:23:07 +0200

    Mancano ancora View e Controller... :(

---
+++ Esercizio1/MVC/Models/DAO.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DAO {
+    public interface IDao {
+        void AggiungiMenu (Menu menu);
+        List<Menu> VisualizzaMenus();
+        Menu VisualizzaMenu (Menu menu);
+    }
+
+    public class DataAccessObject : IDao {
+        public void AggiungiMenu(Menu menu) {}
+
+        public Menu VisualizzaMenu(Menu menu) {
+            throw new NotImplementedException();
+        }
+
+        public List<Menu> VisualizzaMenus() {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Menu {
+        public int Id;
+        public DateTime Giorno {get; set;}
+        public string TipoPasto {get; set;}
+        public List<Pietanza> Pietanze {get; set;}
+        public Menu (DateTime giorno, string tipo, List<Pietanza> pietanze) {
+            this.Giorno = giorno;
+            this.TipoPasto = tipo;
+            this.Pietanze = pietanze;
+        }
+    }
+    public class Pietanza {
+        public int Id;
+        public string NomePietanza {get; set;}
+        public string TipoPietanza {get; set;}
+        public Pietanza (int id, string nome, string tipo) {
+            this.Id = id;
+            this.NomePietanza = nome;
+            this.TipoPietanza = tipo;
+        }
+    }
+
+    public class DatiMock {
+        public List<Menu> MenuMock () {
+            Pietanza piatto1 = new Pietanza(1, "Spaghetti alla Carbonara", "primo");
+            Pietanza piatto2 = new Pietanza(2, "Penne all'Amatriciana", "primo");
+            Pietanza piatto3 = new Pietanza(3, "Risotto Panna e Zafferano", "primo");
+            Pietanza piatto4 = new Pietanza(4, "Minestrone (salutare ma non buono)", "primo");
+            Pietanza piatto5 = new Pietanza(5, "Cotoletta di pollo (che piace tanto a Max e Fede)", "secondo");
+            Pietanza piatto6 = new Pietanza(6, "Omelette alle Erbette", "secondo");
+            Pietanza piatto7 = new Pietanza(7, "Zizzona da 1Kg", "secondo");
+            Pietanza piatto8 = new Pietanza(8, "Maiale Arrosto (per Nauman)", "secondo");
+            Pietanza piatto9 = new Pietanza(9, "Patate Fritte (sempre per Max e Fede)", "contorno");
+            Pietanza piatto10 = new Pietanza(10, "Carote Bollite (salutari, ma un po' meh)", "contorno");
+            Pietanza piatto11 = new Pietanza(11, "Crauti", "contorno");
+            Pietanza piatto12 = new Pietanza(12, "Insalata", "contorno");
+            Pietanza piatto13 = new Pietanza(13, "Tiramisu (per Luca Gent)", "dolce");
+            Pietanza piatto14 = new Pietanza(14, "Torta al Cioccolato (che piace tanto a Matte)", "dolce");
+            Pietanza piatto15 = new Pietanza(15, "Cassata Siciliana (per AlessioTutteCose anche se è assente)", "dolce");
+            Pietanza piatto16 = new Pietanza(16, "Pastiera Napoletana (per Carmen)", "dolce");
+            List<Pietanza> pranzo1 = new List<Pietanza>();
+            List<Pietanza> pranzo2 = new List<Pietanza>();
+            List<Pietanza> cena1 = new List<Pietanza>();
+            List<Pietanza> cena2 = new List<Pietanza>();
+            pranzo1.Add(piatto1);
+            pranzo1.Add(piatto5);
+            pranzo1.Add(piatto9);
+            pranzo1.Add(piatto13);
+            pranzo2.Add(piatto2);
+            pranzo2.Add(piatto6);
+            pranzo2.Add(piatto10);
+            pranzo2.Add(piatto14);
+            cena1.Add(piatto3);
+            cena1.Add(piatto7);
+            cena1.Add(piatto11);
+            cena1.Add(piatto15);
+            cena2.Add(piatto4);
+            cena2.Add(piatto8);
+            cena2.Add(piatto12);
+            cena2.Add(piatto16);
+            Menu menu1 = new Menu(DateTime.Today, "pranzo", pranzo1);
+            Menu menu2 = new Menu(DateTime.Today, "cena", cena1);
+            Menu menu3 = new Menu(new DateTime(2018, 5, 10), "pranzo", pranzo2);
+            Menu menu4 = new Menu(new DateTime(2018, 5, 10), "cena", cena2);
+            List<Menu> menus = new List<Menu>();
+            menus.Add(menu1);
+            menus.Add(menu2);
+            menus.Add(menu3);
+            menus.Add(menu4);
+            return menus;
+        }
+    }
+}
---
+++ Esercizio1/MVC/Models/DomainModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAO;
+
+namespace MVC.Models {
+    public class DomainModel {
+        DataAccessObject dao = new DataAccessObject();
+        public void AggiungiMenu (Menu menu) {
+            try {
+                dao.AggiungiMenu(menu);
+            } catch (SystemException) {
+                throw new Exception("Errore di sistema!");
+            }
+        }
+        public List<Menu> VisualizzaMenus () {
+            List<Menu> result = null;
+            try {
+                result = dao.VisualizzaMenus();
+            } catch (SystemException) {
+                throw new Exception("Errore di sistema!");
+            }
+            return result;
+        }
+        public Menu VisualizzaMenu (Menu menu) {
+            Menu result = null;
+            try {
+                result = dao.VisualizzaMenu(menu);
+            } catch (SystemException) {
+                throw new Exception("Errore di sistema!");
+            }
+            return result;
+        }
+    }
+}
