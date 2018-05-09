f91262067fcd311dda8e16bfad9a967aca86c838
JokerJackassJam <edo.bevione@gmail.com>
2018-05-09 15:15:27 +0200

    Es 1 e 2 finiti e funzionanti

---
+++ Esercizio1/Esercizio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio1 {
+    class Program {
+        static void Main(string[] args) {
+            for (int i=1; i<=15; i++) {
+                Console.WriteLine(2*i);
+            }
+            Console.WriteLine();
+            for (int i=15; i>=1; i--) {
+                Console.WriteLine(2*i-1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Esercizio1/Esercizio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Esercizio2 {
+    class Program {
+        static void Main(string[] args) {
+            int[] count;
+            string frase = Console.ReadLine();
+            char[] charSeparators = new char[] {',', ' ', '.', ':', '\n', ';'};
+            string[] parole;
+            parole = frase.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+            Array.Sort(parole, StringComparer.InvariantCulture);
+            count = new int[parole.Length];
+            int j = 0;
+            for (int i = 1; i<parole.Length; i++) {
+                if (parole[i].CompareTo(parole[i-1])==0) {
+                    count[j]++;
+                } else {
+                    j++;
+                }
+            }
+            j=0;
+            for (int i = 1; i<parole.Length; i++) {
+                if (parole[i].CompareTo(parole[i-1])!=0) {
+                    Console.WriteLine($"{parole[i-1]}: {count[j]+1}");
+                    j++;
+                }
+            }
+            Console.WriteLine($"{parole[parole.Length-1]}: {count[j]+1}");
+            Console.ReadLine();
+        }
+    }
+}
