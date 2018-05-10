a27769558580ed31d1527f6b53463b641981db85
JokerJackassJam <edo.bevione@gmail.com>
2018-05-10 14:49:55 +0200

    finite tutte cose con mock

--- Esercizio1/MVC/Controllers/HomeController.cs
+++ Esercizio1/MVC/Controllers/HomeController.cs
@@ -3,22 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DAO;
+using MVC.Models;
 
 namespace MVC.Controllers {
     public class HomeController : Controller {
+        DomainModel dm = new DomainModel();
+
         public ActionResult Index() {
             return View();
         }
 
-        public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
-
+        public ActionResult AllMenus() {
+            ViewBag.Message = "Ecco tutti i nostri squisiti Menu!";
+            List<Menu> result = dm.VisualizzaMenus();
+            ViewBag.Menus = result;
             return View();
         }
 
-        public ActionResult Contact() {
-            ViewBag.Message = "Your contact page.";
+        public ActionResult MenuGiorno() {
+            ViewBag.Message = "";
+            return View();
+        }
 
+        public ActionResult DoveSiamo() {
+            ViewBag.Message = "";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult MenuGiorno(Menu menu) {
+            ViewBag.Message = "";
+            Menu result = dm.VisualizzaMenu(menu);
+            ViewBag.Menu = result;
             return View();
         }
     }
